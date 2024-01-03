1cda21e324254c372844cf9ac4e665fdb1172969
Bandini2001 <bandinikohare30@gmail.com>
2024-01-03 15:57:09 +0530

    Add cource card

--- src/CoursePage.js
+++ src/CoursePage.js
@@ -1,15 +1,24 @@
 import "./CourcePage.css"
 import React from 'react';
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/js/bootstrap.bundle.min";
 
 const CoursePage = ({ data }) => {
   return (
     <>
-    {/* <div className='poster'>
-
-
-    </div> */}
+    <div className='poster'>
+    <div className="poster-text">
+    <p className="poster-swami-name">{data.instructor.name}</p>
+      <h1 className="poster-title">{data.course.title}</h1>
+    </div>
+    <div className="cource-card">
+        <b><p>Course fees</p></b>
+    <h1>₹{data.course.fee.amount}</h1>
+    <button> Register today </button>
+    </div>
+    </div>
     <div>
-      <h1>{data.course.title}</h1>
+       
      
     </div>
     </>
