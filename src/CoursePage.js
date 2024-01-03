285c4f45565ff17966d5a891e4be25d46c8b9824
Bandini2001 <bandinikohare30@gmail.com>
2024-01-03 16:46:06 +0530

    complete

--- src/CoursePage.js
+++ src/CoursePage.js
@@ -1,5 +1,6 @@
 import "./CourcePage.css"
 import React from 'react';
+import profile from "./asset/profile.jpeg"
 // import "bootstrap/dist/css/bootstrap.min.css";
 // import "bootstrap/dist/js/bootstrap.bundle.min";
 
@@ -17,9 +18,33 @@ const CoursePage = ({ data }) => {
     <button> Register today </button>
     </div>
     </div>
-    <div>
-       
-     
+    <div className="main">
+    <div className="nav">
+       <span>ABOUT</span>
+       <span>INSTRUCTOR</span>
+       <span>REVIEWS</span>
+    </div>
+   
+        <h2>About the course</h2>
+        <p className="">{data.course.about.content}</p>
+        <p className="">{data.course.about.content}</p>
+        <p className="">{data.course.about.content}</p>
+        <p className="">{data.course.about.content}</p>
+
+        <h2>Key topics covered</h2>
+        <ol>
+            <li>{data.course.key_topics.html_content}</li>
+        </ol>
+   
+       <h2>About the instructor</h2>
+       <div className="profile">
+        <img src={profile} alt=" profile" />
+        <p>{data.about_instructor.html_content}</p>
+       </div>
+       <div className="review"><h2>{data.testimonial.text}</h2>
+       <h3>{data.testimonial.reviewer_name}</h3>
+       <p>{data.testimonial.reviewer_designation}</p>
+       </div>
     </div>
     </>
   );
